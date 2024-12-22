2f4e429c99b533d7c419ab0f903560aa4feec419
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-12-23 01:35:39 +0600

    Map Functionality

--- frontend/src/features/dashboard/pages/DashboardPage.jsx
+++ frontend/src/features/dashboard/pages/DashboardPage.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import MapSearch from "../../../components/Map";
+
 const DashboardPage = () => {
+  const [address, setAddress] = useState("");
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>{address}</p>
+      <MapSearch onAddressSelect={({ address }) => setAddress(address)} />
     </div>
   );
 };
